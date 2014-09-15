5489b8c99dabdc1951e4eddba0d8dc7cef2f1e14
Zhenxing Zhu <julian0zzx@gmail.com>
2014-09-15 20:56:16 +0800

    init sub projects
    provider / consumer / wrapper / monitor

---
+++ monitor/src/main/java/org/judu/monitor/Monitor.java
@@ -0,0 +1,16 @@
+package org.judu.monitor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Monitor {
+    
+    private static final Logger _logger = LoggerFactory.getLogger(Monitor.class);
+    
+    public static void main(String[] args) {
+        
+        _logger.info("Judu Monitor is running...");
+        
+    }
+    
+}
